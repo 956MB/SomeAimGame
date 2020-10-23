4ee21cfc56dfbc8c67848572e2c437e7785feb64
Bloumbs <baysicsss@gmail.com>
2020-10-23 04:48:51 -0500

    Removed movementLock/fpsCounter options and added 'showExtraStatsBackgrounds' to ExtraSettings

--- Assets/Scripts/util/saveload/extra/ExtraSettings.cs
+++ Assets/Scripts/util/saveload/extra/ExtraSettings.cs
@@ -4,12 +4,11 @@ public class ExtraSettings : MonoBehaviour {
     public static int gameTimer          = 60;
     public static bool targetSound       = true;
     public static bool uiSound           = true;
-    public static bool movementLock      = true;
-    public static bool fpsCounter        = true;
     public static bool showAAR           = true;
     public static float mouseSensitivity = 2.0f;
     public static bool hideUI            = false;
     public static bool showExtraStats    = false;
+    public static bool showExtraStatsBackgrounds = false;
 
     private static ExtraSettings extraSettings;
     void Awake() { extraSettings = this; }
@@ -36,22 +35,6 @@ public class ExtraSettings : MonoBehaviour {
     /// <param name="setUISound"></param>
     public static void SaveUISoundItem(bool setUISound) {
         uiSound = setUISound;
-        extraSettings.SaveExtraSettings();
-    }
-    /// <summary>
-    /// Saves supplied movement lock bool (setMovementLock) to extra settings object (ExtraSettings), then saves extra settings object.
-    /// </summary>
-    /// <param name="setMovementLock"></param>
-    public static void SaveMovementLockItem(bool setMovementLock) {
-        movementLock = setMovementLock;
-        extraSettings.SaveExtraSettings();
-    }
-    /// <summary>
-    /// Saves supplied FPS counter bool (setFPSCounter) to extra settings object (ExtraSettings), then saves extra settings object.
-    /// </summary>
-    /// <param name="setFPSCounter"></param>
-    public static void SaveFPSCounter(bool setFPSCounter) {
-        fpsCounter = setFPSCounter;
         extraSettings.SaveExtraSettings();
     }
     /// <summary>
@@ -87,29 +70,8 @@ public class ExtraSettings : MonoBehaviour {
         extraSettings.SaveExtraSettings();
     }
 
-    /// <summary>
-    /// Saves default extra settings object (ExtraSettings).
-    /// </summary>
-    /// <param name="setGameTimer"></param>
-    /// <param name="setTargetSound"></param>
-    /// <param name="setUISound"></param>
-    /// <param name="setMovementLock"></param>
-    /// <param name="setFPSCounter"></param>
-    /// <param name="setShowAAR"></param>
-    /// <param name="setMouseSens"></param>
-    /// <param name="setHideUI"></param>
-    /// <param name="setShowExtraStats"></param>
-    public static void SaveAllExtraSettingsDefaults(int setGameTimer, bool setTargetSound, bool setUISound, bool setMovementLock, bool setFPSCounter, 
-    bool setShowAAR, float setMouseSens, bool setHideUI, bool setShowExtraStats) {
-        gameTimer        = setGameTimer;
-        targetSound      = setTargetSound;
-        uiSound          = setUISound;
-        movementLock     = setMovementLock;
-        fpsCounter       = setFPSCounter;
-        showAAR          = setShowAAR;
-        mouseSensitivity = setMouseSens;
-        hideUI           = setHideUI;
-        showExtraStats   = setShowExtraStats;
+    public static void SaveShowExtraStatsBackgrounds(bool setShowExtraStatsBackgrounds) {
+        showExtraStatsBackgrounds = setShowExtraStatsBackgrounds;
         extraSettings.SaveExtraSettings();
     }
 
@@ -119,6 +81,29 @@ public class ExtraSettings : MonoBehaviour {
     public void SaveExtraSettings() { ExtraSaveSystem.SaveExtraSettingsData(this); }
 
     /// <summary>
+    /// Saves default extra settings object (ExtraSettings).
+    /// </summary>
+    /// <param name="setGameTimer"></param>
+    /// <param name="setTargetSound"></param>
+    /// <param name="setUISound"></param>
+    /// <param name="setShowAAR"></param>
+    /// <param name="setMouseSens"></param>
+    /// <param name="setHideUI"></param>
+    /// <param name="setShowExtraStats"></param>
+    public static void SaveAllExtraSettingsDefaults(int setGameTimer, bool setTargetSound, bool setUISound,
+    bool setShowAAR, float setMouseSens, bool setHideUI, bool setShowExtraStats, bool setShowExtraStatsBackgrounds) {
+        gameTimer        = setGameTimer;
+        targetSound      = setTargetSound;
+        uiSound          = setUISound;
+        showAAR          = setShowAAR;
+        mouseSensitivity = setMouseSens;
+        hideUI           = setHideUI;
+        showExtraStats   = setShowExtraStats;
+        showExtraStatsBackgrounds = setShowExtraStatsBackgrounds;
+        extraSettings.SaveExtraSettings();
+    }
+
+    /// <summary>
     /// Loads extra settings data (ExtraSettingsDataSerial) and sets values to this extra settings object (ExtraSettings).
     /// </summary>
     /// <param name="extraData"></param>
@@ -126,11 +111,10 @@ public class ExtraSettings : MonoBehaviour {
         gameTimer        = extraData.gameTimer;
         targetSound      = extraData.targetSound;
         uiSound          = extraData.uiSound;
-        movementLock     = extraData.movementLock;
-        fpsCounter       = extraData.fpsCounter;
         showAAR          = extraData.showAAR;
         mouseSensitivity = extraData.mouseSensitivity;
         hideUI           = extraData.hideUI;
         showExtraStats   = extraData.showExtraStats;
+        showExtraStatsBackgrounds = extraData.showExtraStatsBackgrounds;
     }
 }
--- Assets/Scripts/util/saveload/extra/ExtraSettingsDataSerial.cs
+++ Assets/Scripts/util/saveload/extra/ExtraSettingsDataSerial.cs
@@ -4,22 +4,20 @@ public class ExtraSettingsDataSerial {
     public int gameTimer;
     public bool targetSound;
     public bool uiSound;
-    public bool movementLock;
-    public bool fpsCounter;
     public bool showAAR;
     public float mouseSensitivity;
     public bool hideUI;
     public bool showExtraStats;
+    public bool showExtraStatsBackgrounds;
 
     public ExtraSettingsDataSerial() {
         gameTimer        = ExtraSettings.gameTimer;
         targetSound      = ExtraSettings.targetSound;
         uiSound          = ExtraSettings.uiSound;
-        movementLock     = ExtraSettings.movementLock;
-        fpsCounter       = ExtraSettings.fpsCounter;
         showAAR          = ExtraSettings.showAAR;
         mouseSensitivity = ExtraSettings.mouseSensitivity;
         hideUI           = ExtraSettings.hideUI;
         showExtraStats   = ExtraSettings.showExtraStats;
+        showExtraStatsBackgrounds = ExtraSettings.showExtraStatsBackgrounds;
     }
 }
