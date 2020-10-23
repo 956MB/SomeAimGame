8be25524d003fd2cdbf738475d2138a66b4d0108
Bloumbs <baysicsss@gmail.com>
2020-10-23 05:02:40 -0500

    Changes to KeybindsHandler method to get new keybind for future controls section

--- Assets/Scripts/keybindings/KeybindsHandler.cs
+++ Assets/Scripts/keybindings/KeybindsHandler.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class KeybindsHandler : MonoBehaviour {
+    public static bool getNewKeybind = false;
+    public static string newKeybind = "";
+
     void Update() {
-        
+        if (getNewKeybind) { ReturnNewKeybind(); }
     }
 
-    public static string returnNewKeybind() {
-        while (true) {
-            foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode))) {
-                if (Input.GetKeyUp(vKey)) {
-                    //Debug.Log($"New keypressed: {vKey.ToString()}");
-                    return vKey.ToString();
-                }
+    public static void ReturnNewKeybind() {
+        foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode))) {
+            if (Input.GetKeyUp(vKey)) {
+                //Debug.Log($"New keypressed: {vKey.ToString()}");
+                newKeybind = vKey.ToString();
+                getNewKeybind = false;
             }
         }
     }
