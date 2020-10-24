0fee8facd255c19fdc5598508a6592620ef7ce99
Bloumbs <baysicsss@gmail.com>
2020-10-24 04:55:44 -0500

    Added seperate button hover handler event trigger for user profile thing

---
+++ Assets/Scripts/interface/handlers/ButtonHoverHandler_EventTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ButtonHoverHandler_EventTrigger : MonoBehaviour {
+    public GameObject childBorder;
+    public GameObject parentOptionsObject;
+    public static bool optionsObjectOpen = false;
+
+    private void Start() {
+        foreach (Transform child in parentOptionsObject.transform) {
+            if (child.gameObject.tag == "OptionObjectItem") { child.gameObject.SetActive(false); }
+        }
+    }
+
+    public void EnableBorder() { childBorder.SetActive(true); }
+
+    public void DisableBorder() { childBorder.SetActive(false); }
+
+    public void ToggleOptionsObject() {
+        if (optionsObjectOpen) DisableBorder();
+        else EnableBorder();
+
+        LoopToggleOptions();
+    }
+
+    public void LoopToggleOptions() {
+        foreach (Transform child in parentOptionsObject.transform) {
+            if (child.gameObject.tag == "OptionObjectItem") { child.gameObject.SetActive(!optionsObjectOpen); }
+        }
+
+        optionsObjectOpen = !optionsObjectOpen;
+    }
+}
