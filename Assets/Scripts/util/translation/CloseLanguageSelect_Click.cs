8b2ea03f53244eff1574f9c54fdd233d1e250b43
Bloumbs <baysicsss@gmail.com>
2020-10-25 13:38:49 -0500

    Click handler to close language select if open

---
+++ Assets/Scripts/util/translation/CloseLanguageSelect_Click.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloseLanguageSelect_Click : MonoBehaviour, IPointerClickHandler {
+    public void OnPointerClick(PointerEventData pointerEventData) {
+        if (LanguageSelect.languageSelectOpen) { LanguageSelect.CloseLanguageSelect_Static(); }
+    }
+}
