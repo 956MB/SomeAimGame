e2c5ebc36e92c0bcf98f35a4be0f2d00d7282d1e
Bloumbs <baysicsss@gmail.com>
2020-10-29 04:48:40 -0500

    Dev event stuff

---
+++ Assets/Scripts/util/events/DevEvent.cs
@@ -0,0 +1,31 @@
+﻿
+public class DevEvent {
+    public bool isSelected;
+    public DevEventType eventType;
+    public string eventTime;
+    public string eventContent;
+    public string eventExtra;
+
+    public DevEvent(string eventTime, DevEventType eventType, string eventContent, string eventExtra, bool isSelected) {
+        this.eventTime    = eventTime;
+        this.eventType    = eventType;
+        this.eventContent = eventContent;
+        this.eventExtra   = eventExtra;
+        this.isSelected   = isSelected;
+    }
+}
+
+public enum DevEventType {
+    Gamemode     = 0,
+    Time         = 1,
+    Crosshair    = 2,
+    Targets      = 3,
+    Interface    = 4,
+    Save         = 5,
+    Skybox       = 6,
+    Language     = 7,
+    Keybind      = 8,
+    Sound        = 9,
+    Notification = 10,
+    Stats        = 11,
+}
