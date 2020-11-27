8f2f9ec06daffd49989872f38d87942655502207
Bloumbs <baysicsss@gmail.com>
2020-11-26 21:19:02 -0600

    Method to enable/disable border attached to gameobject script on

---
+++ Assets/Scripts/interface/handlers/ButtonBorderHover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonBorderHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public GameObject borderObj;
+
+    void Start() { borderObj.SetActive(false); }
+
+    public void OnPointerEnter(PointerEventData pointerEventData) {
+        borderObj.SetActive(true);
+
+        if (ToggleHandler.UISoundOn()) { UISound.PlayUISound_HoverInner(); }
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData) { borderObj.SetActive(false); }
+}
