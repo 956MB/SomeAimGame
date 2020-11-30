11d734a09a6190f0701cf9ee90eef7abe990aeb8
Bloumbs <baysicsss@gmail.com>
2020-11-30 11:25:37 -0600

    Adde script to close settings panel from attached button

---
+++ Assets/Scripts/interface/handlers/SettingsPanelClose.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SettingsPanelClose : MonoBehaviour, IPointerClickHandler {
+    public void OnPointerClick(PointerEventData pointerEventData) {
+        SettingsPanel.CloseSettingsPanel();
+    }
+}
