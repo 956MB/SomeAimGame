622af5be42cf5023bcbf30440920dc27f5ba2adc
Bloumbs <baysicsss@gmail.com>
2020-12-03 08:29:25 -0600

    Added script for closing AAR on button click

---
+++ Assets/Scripts/interface/handlers/AARPanelClose.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AARPanelClose : MonoBehaviour, IPointerClickHandler {
+    public void OnPointerClick(PointerEventData pointerEventData) {
+        SettingsPanel.CloseAfterActionReport();
+    }
+}
