e1491e19baa5e29f64ae0e3b289536ecb313dba3
Bloumbs <baysicsss@gmail.com>
2020-12-04 03:54:35 -0600

    Added script tp pause/play game timeScale to stop follow target moving on path

---
+++ Assets/Scripts/util/GameTime.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SomeAimGame.Utilities {
+    public class GameTime : MonoBehaviour {
+        /// <summary>
+        /// Changes game timeScale to 0 (Pause).
+        /// </summary>
+        public static void PauseGameTime() { Time.timeScale = 0; }
+        /// <summary>
+        /// Changes game timeScale to 1 (Play).
+        /// </summary>
+        public static void ContinueGameTime() { Time.timeScale = 1; }
+    }
+}
