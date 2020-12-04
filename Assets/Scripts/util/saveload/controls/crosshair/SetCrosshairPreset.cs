c25e252b40bd1943f7233997513015dc4ec13524
Bloumbs <baysicsss@gmail.com>
2020-12-04 03:57:51 -0600

    Started script for setting preset crosshairs

---
+++ Assets/Scripts/util/saveload/controls/crosshair/SetCrosshairPreset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+using SomeAimGame.Utilities;
+
+public class SetCrosshairPreset : MonoBehaviour, IPointerClickHandler {
+    public void OnPointerClick(PointerEventData pointerEventData) {
+        string clickedPresetName = pointerEventData.pointerCurrentRaycast.gameObject.name;
+
+        CrosshairPresets.SetPresetCrosshair(clickedPresetName);
+
+        NotificationHandler.ShowTimedNotification_Translated("presetcrosshairset", "", InterfaceColors.notificationColorGreen);
+    }
+}
