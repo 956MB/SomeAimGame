a6a689ff031e19855dcc355ca88cdcf1ec7f4e7c
Bloumbs <baysicsss@gmail.com>
2020-12-05 23:08:28 -0600

    Added check to close target sound effect dropdowns

--- Assets/Scripts/util/translation/CloseAction_Click.cs
+++ Assets/Scripts/util/translation/CloseAction_Click.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
+using SomeAimGame.SFX;
+
 public class CloseAction_Click : MonoBehaviour, IPointerClickHandler {
     public void OnPointerClick(PointerEventData pointerEventData) {
         LanguageSelect.CheckCloseLanguageSelect();
+
+        TargetSoundSelect.CheckCloseTargetSoundDropdowns();
 
         QuitGame.CheckCloseQuitConfirmation();
 
