dc02b254e9dc68fca702bb6c672db46b0c597907
Bloumbs <baysicsss@gmail.com>
2020-12-05 23:10:00 -0600

    Added utils class for various dropdown values

---
+++ Assets/Scripts/util/DropdownUtils.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SomeAimGame.Utilities {
+    public class DropdownUtils {
+        public static Vector3 arrowDownScale          = new Vector3(1f, 1f, 1f);
+        public static Vector3 arrowupScale            = new Vector3(1f, -1f, 1f);
+        public static Vector3 dropdownBodyOpenScale   = new Vector3(1f, 1f, 1f);
+        public static Vector3 dropdownBodyClosedScale = new Vector3(1f, 0f, 1f);
+        public static Color32 uiSoundBackgroundOn     = new Color32(255, 255, 255, 15);
+        public static Color32 uiSoundBackgroundOff    = new Color32(255, 255, 255, 0);
+    }
+}
