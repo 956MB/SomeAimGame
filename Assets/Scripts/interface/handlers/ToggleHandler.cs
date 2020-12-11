0fd15b40058b11bb37a96986d056d4ce57095b6d
Bloumbs <baysicsss@gmail.com>
2020-12-11 06:07:12 -0600

    Removed bunch of unnecessary calls to CheckPlayClick_Toggle

--- Assets/Scripts/interface/handlers/ToggleHandler.cs
+++ Assets/Scripts/interface/handlers/ToggleHandler.cs
@@ -24,53 +24,43 @@ public class ToggleHandler : MonoBehaviour {
     /// <param name="toggleClicked"></param>
     public void HandleToggle(Toggle toggleClicked) {
         clickedToggleName = toggleClicked.name;
+        SFXManager.CheckPlayClick_Toggle();
 
         switch (clickedToggleName) {
-            case "TargetSoundToggle": // Toggles target hit sound.
-                SFXManager.CheckPlayClick_Toggle();
-                //TargetSoundSelect.ToggleTargetSoundSelectionContainer_Static();
-                TargetSoundSelect.SetSoundSelectionContainerContainerState(toggleClicked.isOn);
+            // SFX //
 
+            case "TargetSoundToggle": // Toggles target hit sound.
+                TargetSoundSelect.SetSoundSelectionContainerContainerState(toggleClicked.isOn);
                 SFXSettings.SaveTargetSoundOn(toggleClicked.isOn);
                 break;
 
             case "TargetMissSoundToggle": // Toggles target miss sound.
-                SFXManager.CheckPlayClick_Toggle();
-
                 SFXSettings.SaveTargetMissSoundOn(toggleClicked.isOn);
                 break;
 
             case "UISoundToggle": // Toggles UI sound.
-                SFXManager.CheckPlayClick_Toggle();
-
                 SFXSettings.SaveUISoundOn(toggleClicked.isOn);
                 break;
 
             case "AARToggle": // Toggles showing 'AfterActionReport'.
-                SFXManager.CheckPlayClick_Toggle();
-
                 ExtraSettings.SaveShowAAR(toggleClicked.isOn);
                 break;
 
-            case "CenterDotToggle": // Toggles center dot for crosshair.
-                SFXManager.CheckPlayClick_Toggle();
+            // Crosshair //
 
+            case "CenterDotToggle": // Toggles center dot for crosshair.
                 simpleCrosshair.SetCenterDot(toggleClicked.isOn, true);
                 CrosshairSettings.SaveCenterDot(toggleClicked.isOn);
                 CrosshairOptionsObject.SaveCrosshairObject(true);
                 break;
 
             case "TStyleToggle": // Toggles TStyle for crosshair.
-                SFXManager.CheckPlayClick_Toggle();
-
                 simpleCrosshair.SetTStyle(toggleClicked.isOn, true);
                 CrosshairSettings.SaveTStyle(toggleClicked.isOn);
                 CrosshairOptionsObject.SaveCrosshairObject(true);
                 break;
 
             case "OutlineEnableToggle": // Toggles outline for crosshair.
-                SFXManager.CheckPlayClick_Toggle();
-
                 simpleCrosshair.SetOutlineEnabled(toggleClicked.isOn, true);
                 CrosshairSettings.SaveOutlineEnabled(toggleClicked.isOn);
                 CrosshairOptionsObject.SetOutlineContainerState(toggleClicked.isOn);
@@ -78,78 +68,71 @@ public class ToggleHandler : MonoBehaviour {
                 break;
 
             case "ShowExtraStatsToggle": // Toggles 'ExtraStats' panel in 'AfterActionReport'.
-                SFXManager.CheckPlayClick_Toggle();
                 StatsManager.SetExtraStatsState(toggleClicked.isOn);
                 ExtraSettings.SaveShowExtraStats(toggleClicked.isOn);
                 break;
 
             case "ShowExtraStatsBackgroundsToggle": // Toggles 'ExtraStats' backgrounds panel in 'AfterActionReport'.
-                SFXManager.CheckPlayClick_Toggle();
                 StatsManager.SetExtraStatsBackgroundsState(toggleClicked.isOn);
                 ExtraSettings.SaveShowExtraStatsBackgrounds(toggleClicked.isOn);
                 break;
 
             case "QuickStartToggle": // Toggles quick start game in gamemode panel.
-                SFXManager.CheckPlayClick_Toggle();
-
                 CosmeticsSaveSystem.SetQuickStartGame(toggleClicked.isOn);
                 CosmeticsSettings.SaveQuickStartGameItem(toggleClicked.isOn);
                 break;
 
-            case "ShowModeToggle": // Toggles Mode widget.
-                SFXManager.CheckPlayClick_Toggle();
+            // Widgets //
 
+            case "ShowModeToggle": // Toggles Mode widget.
                 modeWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowModeItem(toggleClicked.isOn);
                 break;
 
             case "ShowFPSToggle": // Toggles FPS widget.
-                SFXManager.CheckPlayClick_Toggle();
-
                 fpsWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowFPSItem(toggleClicked.isOn);
                 break;
 
             case "ShowTimeToggle": // Toggles time widget.
-                SFXManager.CheckPlayClick_Toggle();
-
                 timeWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowTimeItem(toggleClicked.isOn);
                 break;
 
             case "ShowScoreToggle": // Toggles score widget.
-                SFXManager.CheckPlayClick_Toggle();
-
                 ScoreWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowScoreItem(toggleClicked.isOn);
                 break;
 
             case "ShowAccuracyToggle": // Toggles accuracy widget.
-                SFXManager.CheckPlayClick_Toggle();
-
                 accuracyWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowAccuracyItem(toggleClicked.isOn);
                 break;
 
             case "ShowStreakToggle": // Toggles streak widget.
-                SFXManager.CheckPlayClick_Toggle();
-
                 streakWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowStreakItem(toggleClicked.isOn);
                 break;
 
             case "ShowTTKToggle": // Toggles ttk widget.
-                SFXManager.CheckPlayClick_Toggle();
-
                 ttkWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowTTKItem(toggleClicked.isOn);
                 break;
 
             case "ShowKPSToggle": // Toggles kps widget.
-                SFXManager.CheckPlayClick_Toggle();
-
                 kpsWidget.SetActive(toggleClicked.isOn);
                 WidgetSettings.SaveShowKPSItem(toggleClicked.isOn);
+                break;
+
+            // Video //
+
+            case "VSyncToggle": // Toggles VSync in video settings.
+                break;
+
+            case "VignetteToggle": // Toggles vignette in video settings.
+                break;
+
+            case "ChromaticAberrationToggle": // Toggles cromatic aberration in video settings.
                 break;
         }
     }
@@ -162,25 +145,4 @@ public class ToggleHandler : MonoBehaviour {
         backgroundToggle.isOn = !backgroundToggle.isOn;
         HandleToggle(backgroundToggle);
     }
-
-    /// <summary>
-    /// Returns ExtraSettings.targetSound value.
-    /// </summary>
-    /// <returns></returns>
-    public static bool TargetSoundOn() { return true; }
-    /// <summary>
-    /// Returns ExtraSettings.uiSound value.
-    /// </summary>
-    /// <returns></returns>
-    public static bool UISoundOn() { return true; }
-    /// <summary>
-    /// Returns WidgetSettings.showFPS value.
-    /// </summary>
-    /// <returns></returns>
-    public static bool FPSCounterOn() { return WidgetSettings.showFPS; }
-    /// <summary>
-    /// Returns ExtraSettings.showAAR value.
-    /// </summary>
-    /// <returns></returns>
-    public static bool ShowAAROn() { return ExtraSettings.showAAR; }
 }
