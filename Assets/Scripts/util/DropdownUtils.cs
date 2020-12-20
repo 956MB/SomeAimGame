1777b226fc3067a10d41d2051203acb2843526ca
Bloumbs <baysicsss@gmail.com>
2020-12-19 22:28:57 -0600

    Added more methods to DropdownUtils

--- Assets/Scripts/util/DropdownUtils.cs
+++ Assets/Scripts/util/DropdownUtils.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using TMPro;
 
+using SomeAimGame.Core.Video;
+
 namespace SomeAimGame.Utilities {
-    public class DropdownUtils {
+    public class DropdownUtils : MonoBehaviour {
         public static Vector3 arrowDownScale          = new Vector3(1f, 1f, 1f);
         public static Vector3 arrowupScale            = new Vector3(1f, -1f, 1f);
         public static Vector3 dropdownBodyOpenScale   = new Vector3(1f, 1f, 1f);
@@ -10,6 +12,13 @@ namespace SomeAimGame.Utilities {
         public static Color32 uiSoundBackgroundOn     = new Color32(255, 255, 255, 15);
         public static Color32 uiSoundBackgroundOff    = new Color32(255, 255, 255, 0);
 
+        /// <summary>
+        /// Calls 'DropdownAction_Open' OR 'DropdownAction_Close' based on supplied dropdown state bool (dropdownOpen).
+        /// </summary>
+        /// <param name="dropdownOpen"></param>
+        /// <param name="dropdownBody"></param>
+        /// <param name="arrowText"></param>
+        /// <param name="dropdownOpenRef"></param>
         public static void SetDropdownState(bool dropdownOpen, GameObject dropdownBody, TMP_Text arrowText, ref bool dropdownOpenRef) {
             if (dropdownOpen) {
                 DropdownAction_Open(dropdownBody, arrowText, ref dropdownOpenRef);
@@ -18,17 +27,97 @@ namespace SomeAimGame.Utilities {
             }
         }
 
+        /// <summary>
+        /// Sets supplied dropdown body GameObject (dropdownBody) state opened.
+        /// </summary>
+        /// <param name="dropdownBody"></param>
+        /// <param name="arrowText"></param>
+        /// <param name="dropdownOpenRef"></param>
         public static void DropdownAction_Open(GameObject dropdownBody, TMP_Text arrowText, ref bool dropdownOpenRef) {
             dropdownBody.transform.localScale = dropdownBodyOpenScale;
             arrowText.transform.localScale    = arrowupScale;
             dropdownOpenRef                   = true;
             Util.RefreshRootLayoutGroup(dropdownBody);
         }
-
+        /// <summary>
+        /// Sets supplied dropdown body GameObject (dropdownBody) state closed.
+        /// </summary>
+        /// <param name="dropdownBody"></param>
+        /// <param name="arrowText"></param>
+        /// <param name="dropdownOpenRef"></param>
         public static void DropdownAction_Close(GameObject dropdownBody, TMP_Text arrowText, ref bool dropdownOpenRef) {
             dropdownBody.transform.localScale = dropdownBodyClosedScale;
             arrowText.transform.localScale    = arrowDownScale;
             dropdownOpenRef                   = false;
+        }
+
+        /// <summary>
+        /// Calls 'CreateDropdownItem' in loop to create dropdown items with supplied dropdown item prefab (dropdownItemPrefab).
+        /// </summary>
+        /// <param name="videoSettingType"></param>
+        /// <param name="dropdownItemPrefab"></param>
+        /// <param name="parentDropdownPrefab"></param>
+        /// <param name="stringsArray"></param>
+        public static void CreateDropdownItems_Loop(VideoDropdowns videoSettingType, GameObject dropdownItemPrefab, GameObject parentDropdownPrefab, params string[] stringsArray) {
+            for (int i = 0; i < stringsArray.Length; i++) {
+                CreateDropdownItem((int)videoSettingType, i, stringsArray[i], dropdownItemPrefab, parentDropdownPrefab);
+            }
+        }
+        /// <summary>
+        /// Calls 'CreateDropdownItem' in loop to create dropdown items with supplied dropdown item prefab (dropdownItemPrefab).
+        /// </summary>
+        /// <param name="videoSettingType"></param>
+        /// <param name="dropdownItemPrefab"></param>
+        /// <param name="parentDropdownPrefab"></param>
+        /// <param name="enumsArray"></param>
+        public static void CreateDropdownItems_Loop(VideoDropdowns videoSettingType, GameObject dropdownItemPrefab, GameObject parentDropdownPrefab, params DisplayModes[] enumsArray) {
+            foreach (DisplayModes itemType in enumsArray) { CreateDropdownItem((int)videoSettingType, (int)itemType, dropdownItemPrefab, parentDropdownPrefab); }
+        }
+        /// <summary>
+        /// Calls 'CreateDropdownItem' in loop to create dropdown items with supplied dropdown item prefab (dropdownItemPrefab).
+        /// </summary>
+        /// <param name="videoSettingType"></param>
+        /// <param name="dropdownItemPrefab"></param>
+        /// <param name="parentDropdownPrefab"></param>
+        /// <param name="enumsArray"></param>
+        public static void CreateDropdownItems_Loop(VideoDropdowns videoSettingType, GameObject dropdownItemPrefab, GameObject parentDropdownPrefab, params AntiAliasType[] enumsArray) {
+            foreach (AntiAliasType itemType in enumsArray) { CreateDropdownItem((int)videoSettingType, (int)itemType, dropdownItemPrefab, parentDropdownPrefab); }
+        }
+
+        /// <summary>
+        /// Creates dropdown item inside parent dropdown body (parentDropdownPrefab), and sets dropdown values from supplied ints (setDropdownInt/setSettingInt).
+        /// </summary>
+        /// <param name="setDropdownInt"></param>
+        /// <param name="setSettingInt"></param>
+        /// <param name="dropdownItemPrefab"></param>
+        /// <param name="parentDropdownPrefab"></param>
+        public static void CreateDropdownItem(int setDropdownInt, int setSettingInt, GameObject dropdownItemPrefab, GameObject parentDropdownPrefab) {
+            GameObject dropdownItem = Instantiate(dropdownItemPrefab);
+            dropdownItem.GetComponent<VideoSettingChange>().SetDropdownInt(setDropdownInt, setSettingInt);
+
+            TMP_Text itemText = dropdownItem.GetComponentsInChildren<TMP_Text>()[0];
+
+            if ((VideoDropdowns)setDropdownInt == VideoDropdowns.DISPLAY_MODE) { itemText.SetText($"{VideoSettingUtil.ReturnTypeString((DisplayModes)setSettingInt)}"); }
+            if ((VideoDropdowns)setDropdownInt == VideoDropdowns.ANTI_ALIASING) { itemText.SetText($"{VideoSettingUtil.ReturnTypeString((AntiAliasType)setSettingInt)}"); }
+
+            dropdownItem.transform.SetParent(parentDropdownPrefab.transform);
+        }
+        /// <summary>
+        /// Creates dropdown item inside parent dropdown body (parentDropdownPrefab), and sets dropdown values from supplied ints (setDropdownInt/setSettingInt).
+        /// </summary>
+        /// <param name="setDropdownInt"></param>
+        /// <param name="setSettingInt"></param>
+        /// <param name="setDropdownText"></param>
+        /// <param name="dropdownItemPrefab"></param>
+        /// <param name="parentDropdownPrefab"></param>
+        public static void CreateDropdownItem(int setDropdownInt, int setSettingInt, string setDropdownText, GameObject dropdownItemPrefab, GameObject parentDropdownPrefab) {
+            GameObject dropdownItem = Instantiate(dropdownItemPrefab);
+            dropdownItem.GetComponent<VideoSettingChange>().SetDropdownInt(setDropdownInt, setSettingInt);
+
+            TMP_Text itemText = dropdownItem.GetComponentsInChildren<TMP_Text>()[0];
+            itemText.SetText($"{setDropdownText}");
+
+            dropdownItem.transform.SetParent(parentDropdownPrefab.transform);
         }
     }
 }
